641eaa524ce11036d1a5441ea1584a1e857abc49
Pedro Lazari <plazari96@gmail.com>
2023-11-22 11:07:09 -0300

    Adding Prometheus and MKTXP Addon

---
+++ mktxp-addon/run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# Home Assistant Community Add-on: Amazon S3 Backup
+# ==============================================================================
+#bashio::log.level "debug"
+
+bashio::log.info "Starting MKTXP"
+
+mktxp export
---
+++ prometheus-addon/CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+## 1.0.0
+
+- Initial release:
+  - Do the same as Thomasfr did until 8 months ago
+
